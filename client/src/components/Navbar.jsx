4ea881f773419703e27675298ee9510bacbbd58a
Thundem <vovkandrij7@gmail.com>
2025-03-25 22:22:45 +0200

    added Navbar and PrivateRoute components for authentication and navigation

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../redux/slices/authSlice';
+
+const Navbar = () => {
+  const { isAuthenticated, user } = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <nav>
+      <div>
+        <Link to="/">Smart Bin Моніторинг</Link>
+        {isAuthenticated ? (
+          <>
+            <Link to="/">Головна</Link>
+            <Link to="/profile">Профіль</Link>
+            <span>Вітаємо, {user.username}</span>
+            <button onClick={handleLogout}>Вийти</button>
+          </>
+        ) : (
+          <>
+            <Link to="/login">Увійти</Link>
+            <Link to="/register">Реєстрація</Link>
+          </>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
