75ca962297c5ab2cdff1846dfbdadcf62cf7aabd
Thundem <vovkandrij7@gmail.com>
2025-03-25 22:20:38 +0200

    added initial client setup with Vite, React, and basic routing

---
+++ client/src/App.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import PrivateRoute from './components/PrivateRoute';
+import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import DeviceDetail from './pages/DeviceDetail';
+import Profile from './pages/Profile';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route 
+              path="/" 
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              } 
+            />
+            <Route 
+              path="/device/:id" 
+              element={
+                <PrivateRoute>
+                  <DeviceDetail />
+                </PrivateRoute>
+              } 
+            />
+            <Route 
+              path="/profile" 
+              element={
+                <PrivateRoute>
+                  <Profile />
+                </PrivateRoute>
+              } 
+            />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
