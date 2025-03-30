bd8dcf765fb0038a9a39b20ff09f5e7e4aef8696
Thundem <vovkandrij7@gmail.com>
2025-03-30 21:20:35 +0300

    fixed problem with undefine process

--- client/src/utils/axios.jsx
+++ client/src/utils/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000'
+  baseURL: import.meta.env.APP_API_URL || 'http://localhost:3000'
 });
 
 instance.interceptors.request.use(
