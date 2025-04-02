91f8e59ffd63fe7a1324605faf19bcd674243106
Thundem <vovkandrij7@gmail.com>
2025-04-03 00:21:51 +0300

    added user routes to server for user management

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const connectDatabase = require('./config/database');
 const dashboardController = require('./controllers/dashboardController');
 const authRoutes = require('./routes/authRoutes');
 const deviceRoutes = require('./routes/deviceRoutes');
+const userRoutes = require('./routes/userRoutes');
 const dashboardRoutes = require('./routes/dashboardRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -22,6 +23,7 @@ app.use(express.json());
 // Маршрути
 app.use('/api/auth', authRoutes);
 app.use('/api/devices', deviceRoutes);
+app.use('/api/users', userRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 
 // Глобальний обробник помилок
