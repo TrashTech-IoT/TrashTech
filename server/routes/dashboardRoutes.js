be537b541b2af005f2d13656b604a85b50293523
VladyslavGreschuk <wladhre@gmail.com>
2025-04-09 01:39:39 +0300

    added routes for getting user devices and deleting a device in dashboardRoutes

--- server/routes/dashboardRoutes.js
+++ server/routes/dashboardRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createDevice, getDeviceInfo, getFillLevelHistory, getFillLevel} = require('../controllers/dashboardController'); // Import the getDeviceInfo function
+const { createDevice, getDeviceInfo, getFillLevelHistory, getFillLevel, getUserDevices, deleteDevice } = require('../controllers/dashboardController'); // Import the getDeviceInfo function
 const { authMiddleware } = require('../middleware/auth');
 
 // Route to create a new device
@@ -14,5 +14,10 @@ router.get('/device/:serialNumber/fillLevel', authMiddleware, getFillLevel);
 
 // Route to get the fillLevelHistory of a device
 router.get('/device/:serialNumber/fillLevelHistory', authMiddleware, getFillLevelHistory);
+
+// Route to get all devices for the authenticated user
+router.get('/devices', authMiddleware, getUserDevices);
+
+router.delete('/device-delete', authMiddleware, deleteDevice);
 
 module.exports = router;
